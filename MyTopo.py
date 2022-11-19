449551b6751b52ba64170273e08099bb82be9bfd
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2022-11-19 18:05:40 -0500

    Update MyTopo.py

--- MyTopo.py
+++ MyTopo.py
@@ -1,8 +1,23 @@
-"""
-Example topology of Quagga routers
-"""
-
 from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.node import Node
+from mininet.log import setLogLevel, info
+from mininet.cli import CLI
+
+
+class LinuxRouter( Node ):
+    "A Node with IP forwarding enabled."
+
+    # pylint: disable=arguments-differ
+    def config( self, **params ):
+        super( LinuxRouter, self).config( **params )
+        # Enable forwarding on the router
+        self.cmd( 'sysctl net.ipv4.ip_forward=1' )
+
+    def terminate( self ):
+        self.cmd( 'sysctl net.ipv4.ip_forward=0' )
+        super( LinuxRouter, self ).terminate()
+    
 class MyTopo(Topo):
 
     "Creates a topology of Quagga routers"
@@ -14,34 +29,51 @@ class MyTopo(Topo):
 
         # Directory where this file / script is located"
 
-
+        h1 = self.addHost( 'H1', ip='170.16.0.1/16', defaultRoute='via 170.16.0.2/16' )
+        r1 = self.addNode( 'R1', cls=LinuxRouter, ip='170.16.0.2/16' )
+        r2 = self.addNode( 'R2', cls=LinuxRouter, ip='171.16.0.2/16' )
+        r3 = self.addNode( 'R3', cls=LinuxRouter, ip='172.16.0.2/16' )
+        r4 = self.addNode( 'R4', cls=LinuxRouter, ip='175.16.0.2/16' )
+        h2 = self.addHost( 'H2', ip='175.16.0.1/16', defaultRoute='via 175.16.0.2/16' )
         # List of Quagga host configs
-        hosts = []
-        hosts.append(self.addHost(name='H1', ip='172.0.1.1/16', loIP='10.0.1.1/24'))
-        hosts.append(self.addHost(name='R1', ip='172.0.1.2/16', loIP='10.0.2.1/24'))
-        hosts.append(self.addHost(name='R2', ip='173.0.1.2/16', loIP='10.0.3.1/24'))
-        hosts.append(self.addHost(name='R3', ip='173.0.1.2/16', loIP=None))
-        hosts.append(self.addHost(name='R4', ip='173.0.1.2/16', loIP='10.0.3.1/24'))
-        hosts.append(self.addHost(name='H2', ip='173.0.1.1/16', loIP='10.0.3.1/24'))
-        #quaggaHosts.append(QuaggaHost(name='R4', ip='177.0.1.1/16',
-        #                              loIP='10.0.4.1/24'))
-        #quaggaHosts.append(QuaggaHost(name='H2', ip='177.0.1.2/16',
-        #                              loIP=None))
-
-        # Add switch for IXP fabric
-        # ixpfabric = self.addSwitch('fabric-sw1')
         
+        self.addLink( r1, h1, intfName2='r1-eth0',
+                      params2={ 'ip' : '170.16.0.2/16' } ) 
+        self.addLink( r1, r2, intfName2='r1-eth1',
+                      params2={ 'ip' : '171.16.0.1/16' } )
+        self.addLink( r1, r3, intfName2='r1-eth2',
+                      params2={ 'ip' : '172.16.0.1/16' } )
+        
+        self.addLink( r2, r1, intfName2='r2-eth0',
+                      params2={ 'ip' : '171.16.0.2/16' } ) 
+        self.addLink( r2, r4, intfName2='r2-eth1',
+                      params2={ 'ip' : '173.16.0.1/16' } )
 
-            # Add a loopback interface with an IP in router's announced range
-            # self.addNodeLoopbackIntf(node=host.name, ip=host.loIP)
+        self.addLink( r3, r1, intfName2='r3-eth0',
+                      params2={ 'ip' : '172.16.0.2/16' } ) 
+        self.addLink( r3, r4, intfName2='r3-eth1',
+                      params2={ 'ip' : '174.16.0.1/16' } )
+        
+        self.addLink( r4, r2, intfName2='r4-eth0',
+                      params2={ 'ip' : '173.16.0.2/16' } ) 
+        self.addLink( r4, r3, intfName2='r4-eth1',
+                      params2={ 'ip' : '174.16.0.2/16' } )
+        self.addLink( r4, h2, intfName2='r4-eth2',
+                      params2={ 'ip' : '175.16.0.2/16' } )
+        
+def run():
+    "Test linux router"
+    topo = MyTopo()
+    net = Mininet(topo=topo, waitConnected=True )
+    net.start()
+    info( '*** Routing Table on Router:\n' )
+    info( net[ 'r1' ].cmd( 'route1' ) )
+    info( net[ 'r2' ].cmd( 'route2' ) )
+    info( net[ 'r3' ].cmd( 'route3' ) )
+    info( net[ 'r4' ].cmd( 'route4' ) )
+    CLI( net )
+    net.stop()
 
-            # Configure and setup the Quagga service for this node
-
-        # Attach the quaggaContainer to the IXP Fabric Switch
-        self.addLink(hosts[0], hosts[1])
-        self.addLink(hosts[5], hosts[4])
-        self.addLink(hosts[1], hosts[2])
-        self.addLink(hosts[1], hosts[3])
-        self.addLink(hosts[2], hosts[4])
-        self.addLink(hosts[3], hosts[4])
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    run()
