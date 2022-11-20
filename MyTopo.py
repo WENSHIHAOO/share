b6feb4dac6c81242bb38d506f0013ec1329c595a
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2022-11-20 15:55:29 -0500

    Create MyTopo.py

--- MyTopo.py
+++ MyTopo.py
@@ -33,8 +33,8 @@ class MyTopo(Topo):
         r1 = self.addHost( 'R1', cls=LinuxRouter, ip='170.16.0.2/16' )
         r2 = self.addHost( 'R2', cls=LinuxRouter, ip='171.16.0.2/16' )
         r3 = self.addHost( 'R3', cls=LinuxRouter, ip='172.16.0.2/16' )
-        r4 = self.addHost( 'R4', cls=LinuxRouter, ip='175.16.0.2/16' )
-        h2 = self.addHost( 'H2', ip='175.16.0.1/16', defaultRoute='via 175.16.0.2' )
+        r4 = self.addHost( 'R4', cls=LinuxRouter, ip='173.16.0.2/16' )
+        h2 = self.addHost( 'H2', ip='173.16.0.1/16', defaultRoute='via 173.16.0.2' )
         # List of Quagga host configs
         
         self.addLink(h1, r1, intfName1='H1-eth0', intfName2='R1-eth0',
@@ -43,11 +43,11 @@ class MyTopo(Topo):
                      params1={'ip': '171.16.0.1/16'},params2={'ip': '171.16.0.2/16'})
         self.addLink( r1, r3, intfName1='R1-eth2', intfName2='R3-eth0',
                      params1={'ip': '172.16.0.1/16'},params2={'ip': '172.16.0.2/16'})
-        self.addLink( r2, r4, intfName1='R2-eth1', intfName2='R4-eth0',
+        self.addLink( h2, r4, intfName1='H2-eth0', intfName2='R4-eth0',
                      params1={'ip': '173.16.0.1/16'},params2={'ip': '173.16.0.2/16'})
-        self.addLink( r3, r4, intfName1='R3-eth1', intfName2='R4-eth1',
+        self.addLink( r4, r2, intfName1='R4-eth1', intfName2='R2-eth1',
                      params1={'ip': '174.16.0.1/16'},params2={'ip': '174.16.0.2/16'})
-        self.addLink( h2, r4, intfName1='H2-eth0', intfName2='R4-eth2',
+        self.addLink( r4, r3, intfName1='R4-eth2', intfName2='R3-eth1',
                      params1={'ip': '175.16.0.1/16'},params2={'ip': '175.16.0.2/16'})
 def run():
     "Test linux router"
@@ -55,10 +55,10 @@ def run():
     net = Mininet(topo=topo, waitConnected=True )
     net.start()
     info( '*** Routing Table on Router:\n' )
-#     info( net[ 'R1' ].cmd( 'route' ) )
-#     info( net[ 'R2' ].cmd( 'route' ) )
-#     info( net[ 'R3' ].cmd( 'route' ) )
-#     info( net[ 'R4' ].cmd( 'route' ) )
+    info( net[ 'R1' ].cmd( 'route' ) )
+    info( net[ 'R2' ].cmd( 'route' ) )
+    info( net[ 'R3' ].cmd( 'route' ) )
+    info( net[ 'R4' ].cmd( 'route' ) )
 
     CLI( net )
     net.stop()
