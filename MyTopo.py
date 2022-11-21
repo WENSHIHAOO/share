d1664c9060803df99792486860f4858d62581617
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2022-11-20 23:16:00 -0500

    Update MyTopo.py

--- MyTopo.py
+++ MyTopo.py
@@ -19,19 +19,19 @@ net = None
 class MyTopo(Topo):
 
     def build(self):   
-        H1=self.addHost('H1', cls=LinuxRouter, ip='170.0.0.1/24')
-        R1=self.addHost('R1', cls=LinuxRouter, ip='170.0.0.2/24')
-        R2=self.addHost('R2', cls=LinuxRouter, ip='173.0.0.1/24')
-        R3=self.addHost('R3', cls=LinuxRouter, ip='174.0.0.1/24')
-        R4=self.addHost('R4', cls=LinuxRouter, ip='175.0.0.2/24')
-        H2=self.addHost('H2', cls=LinuxRouter, ip='175.0.0.1/24')
+        H1=self.addHost('H1', cls=LinuxRouter, ip='170.0.0.1/16')
+        R1=self.addHost('R1', cls=LinuxRouter, ip='170.0.0.2/16')
+        R2=self.addHost('R2', cls=LinuxRouter, ip='173.0.0.1/16')
+        R3=self.addHost('R3', cls=LinuxRouter, ip='174.0.0.1/16')
+        R4=self.addHost('R4', cls=LinuxRouter, ip='175.0.0.2/16')
+        H2=self.addHost('H2', cls=LinuxRouter, ip='175.0.0.1/16')
         
         self.addLink(H1,R1)
+        self.addLink(R4,H2)
         self.addLink(R2,R4)
         self.addLink(R3,R4)
         self.addLink(R1,R2)
         self.addLink(R1,R3)
-        self.addLink(R4,H2)
     
 def run():
     topo = MyTopo()
@@ -48,23 +48,23 @@ def run():
     net.get("H1").cmd("route add default gw 170.0.0.2")
     net.get("H2").cmd("route add default gw 175.0.0.2")
     
-    net.get("R1").cmd("ip route add 173.0.0.0/24 via 171.0.0.2")
-    net.get("R1").cmd("ip route add 175.0.0.0/24 via 171.0.0.2")
-    net.get("R1").cmd("ip route add 174.0.0.0/24 via 172.0.0.2")
+    net.get("R1").cmd("ip route add 173.0.0.0/16 via 171.0.0.2")
+    net.get("R1").cmd("ip route add 175.0.0.0/16 via 171.0.0.2")
+    net.get("R1").cmd("ip route add 174.0.0.0/16 via 172.0.0.2")
     
-    net.get("R2").cmd("ip route add 170.0.0.0/24 via 171.0.0.1")
-    net.get("R2").cmd("ip route add 172.0.0.0/24 via 171.0.0.1")
-    net.get("R2").cmd("ip route add 174.0.0.0/24 via 173.0.0.2")
-    net.get("R2").cmd("ip route add 175.0.0.0/24 via 173.0.0.2")
+    net.get("R2").cmd("ip route add 170.0.0.0/16 via 171.0.0.1")
+    net.get("R2").cmd("ip route add 172.0.0.0/16 via 171.0.0.1")
+    net.get("R2").cmd("ip route add 174.0.0.0/16 via 173.0.0.2")
+    net.get("R2").cmd("ip route add 175.0.0.0/16 via 173.0.0.2")
     
-    net.get("R3").cmd("ip route add 170.0.0.0/24 via 172.0.0.1")
-    net.get("R3").cmd("ip route add 171.0.0.0/24 via 172.0.0.1")
-    net.get("R3").cmd("ip route add 173.0.0.0/24 via 174.0.0.2")
-    net.get("R3").cmd("ip route add 175.0.0.0/24 via 174.0.0.2")
+    net.get("R3").cmd("ip route add 170.0.0.0/16 via 172.0.0.1")
+    net.get("R3").cmd("ip route add 171.0.0.0/16 via 172.0.0.1")
+    net.get("R3").cmd("ip route add 173.0.0.0/16 via 174.0.0.2")
+    net.get("R3").cmd("ip route add 175.0.0.0/16 via 174.0.0.2")
 
-    net.get("R4").cmd("ip route add 170.0.0.0/24 via 173.0.0.1")
-    net.get("R4").cmd("ip route add 171.0.0.0/24 via 173.0.0.1")
-    net.get("R4").cmd("ip route add 172.0.0.0/24 via 174.0.0.1")
+    net.get("R4").cmd("ip route add 170.0.0.0/16 via 173.0.0.1")
+    net.get("R4").cmd("ip route add 171.0.0.0/16 via 173.0.0.1")
+    net.get("R4").cmd("ip route add 172.0.0.0/16 via 174.0.0.1")
     
     info('** Running CLI\n')
     CLI(net)
