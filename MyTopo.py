51168a3978e49469b11df9f39f12a0d631344c66
WENSHIHAOO <99094977+WENSHIHAOO@users.noreply.github.com>
2022-11-21 16:11:09 -0500

    Update MyTopo.py

--- MyTopo.py
+++ MyTopo.py
@@ -1,7 +1,7 @@
 from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.node import Node
-from mininet.log import setLogLevel, info
+from mininet.log import setLogLevel
 from mininet.cli import CLI
 
 class LinuxRouter( Node ):
@@ -18,12 +18,12 @@ class LinuxRouter( Node ):
 class MyTopo(Topo):
 
     def build(self):   
-        H1=self.addHost('H1', cls=LinuxRouter, ip='170.0.0.1/16')
-        R1=self.addHost('R1', cls=LinuxRouter, ip='170.0.0.2/16')
-        R2=self.addHost('R2', cls=LinuxRouter, ip='173.0.0.1/16')
-        R3=self.addHost('R3', cls=LinuxRouter, ip='174.0.0.1/16')
-        R4=self.addHost('R4', cls=LinuxRouter, ip='175.0.0.2/16')
-        H2=self.addHost('H2', cls=LinuxRouter, ip='175.0.0.1/16')
+        H1=self.addHost('H1', ip='170.0.0.1/16')
+        R1=self.addNode('R1', cls=LinuxRouter, ip='170.0.0.2/16')
+        R2=self.addNode('R2', cls=LinuxRouter, ip='173.0.0.1/16')
+        R3=self.addNode('R3', cls=LinuxRouter, ip='174.0.0.1/16')
+        R4=self.addNode('R4', cls=LinuxRouter, ip='175.0.0.2/16')
+        H2=self.addHost('H2', ip='175.0.0.1/16')
         
         self.addLink(H1,R1)
         self.addLink(R4,H2)
@@ -48,8 +48,8 @@ def run():
     net.get("H2").cmd("route add default gw 175.0.0.2")
     
     net.get("R1").cmd("ip route add 173.0.0.0/16 via 171.0.0.2")
-    net.get("R1").cmd("ip route add 175.0.0.0/16 via 171.0.0.2")
     net.get("R1").cmd("ip route add 174.0.0.0/16 via 172.0.0.2")
+    net.get("R1").cmd("ip route add 175.0.0.0/16 via 172.0.0.2")
     
     net.get("R2").cmd("ip route add 170.0.0.0/16 via 171.0.0.1")
     net.get("R2").cmd("ip route add 172.0.0.0/16 via 171.0.0.1")
